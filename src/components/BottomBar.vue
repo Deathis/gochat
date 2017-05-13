430d3964b0d65e7b61f7d0f9996f25b0ca15d4c3
Randy Ou <ourd2012@gmail.com>
2017-05-13 17:48:46 +0800

    增加BottomBar点击时导航到对应路径。

--- src/components/BottomBar.vue
+++ src/components/BottomBar.vue
@@ -1,8 +1,41 @@
 <template>
-    <md-bottom-bar md-shift>
-        <md-bottom-bar-item md-icon="message">Massage</md-bottom-bar-item>
-        <md-bottom-bar-item md-icon="contacts">Contacts</md-bottom-bar-item>
-        <md-bottom-bar-item md-icon="explore">Explore</md-bottom-bar-item>
-        <md-bottom-bar-item md-icon="person">Me</md-bottom-bar-item>
+    <md-bottom-bar md-shift @change="change">
+        <md-bottom-bar-item md-icon="message" :md-active="path==='/talk'">Massage</md-bottom-bar-item>
+        <md-bottom-bar-item md-icon="contacts" :md-active="path==='/contact'">Contacts</md-bottom-bar-item>
+        <md-bottom-bar-item md-icon="explore" :md-active="path==='/explore'">Explore</md-bottom-bar-item>
+        <md-bottom-bar-item md-icon="person" :md-active="path==='/me'">Me</md-bottom-bar-item>
     </md-bottom-bar>
 </template>
+
+<script>
+export default {
+  name: 'bottomBar',
+  methods: {
+    change(index) {
+      let path = '/';
+      switch (index) {
+        case 0:
+          path = '/talk';
+          break;
+        case 1:
+          path = '/contact';
+          break;
+        case 2:
+          path = '/explore';
+          break;
+        case 3:
+          path = '/me';
+          break;
+        default:
+          break;
+      }
+      this.$router.push(path);
+    },
+  },
+  computed: {
+    path() {
+      return this.$route.path;
+    },
+  },
+};
+</script>
