143833542c3c204325f44e61b5de6bc6283bf047
Randy Ou <ourd2012@gmail.com>
2017-05-13 16:25:06 +0800

    add TopBar

---
+++ src/components/TopBar.vue
@@ -0,0 +1,26 @@
+<template>
+    <md-toolbar>
+        <span>gochat</span>
+        <md-button class="md-icon-button">
+            <md-icon>search</md-icon>
+        </md-button>
+        <md-button class="md-icon-button">
+            <md-icon>add</md-icon>
+        </md-button>
+    </md-toolbar>
+</template>
+
+<script>
+export default {
+  name: 'topBar',
+};
+</script>
+
+<style lang="scss" scoped>
+@import '~compass-mixins/compass/css3';
+
+span {
+    @include flex(1);
+}
+</style>
+
