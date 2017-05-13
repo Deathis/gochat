860250e0bea3e91fd7fcb93c08d3080966b1488f
Randy Ou <ourd2012@gmail.com>
2017-05-13 16:29:16 +0800

    Add BottomBar

---
+++ src/components/BottomBar.vue
@@ -0,0 +1,8 @@
+<template>
+    <md-bottom-bar md-shift>
+        <md-bottom-bar-item md-icon="message">Massage</md-bottom-bar-item>
+        <md-bottom-bar-item md-icon="contacts">Contacts</md-bottom-bar-item>
+        <md-bottom-bar-item md-icon="explore">Explore</md-bottom-bar-item>
+        <md-bottom-bar-item md-icon="person">Me</md-bottom-bar-item>
+    </md-bottom-bar>
+</template>
