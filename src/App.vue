fadae17fcd4c918f1c50a021656bcabb9c791bdf
Randy Ou <ourd2012@gmail.com>
2017-05-13 16:37:34 +0800

    add route to TalkPanel and set app style

--- src/App.vue
+++ src/App.vue
@@ -11,5 +11,12 @@ export default {
 </script>
 
 <style lang="scss">
+html,
+body {
+  height: 100%;
+}
 
+#app {
+  height: 100%;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,15 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import TalkPanel from '@/components/TalkPanel';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
-
+    {
+      path: '/talk',
+      name: 'talk',
+      component: TalkPanel,
+    },
   ],
 });
