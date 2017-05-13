e60a22070200900e1edc588fc99f619fb76f71a0
Randy Ou <ourd2012@gmail.com>
2017-05-13 16:32:34 +0800

    add TalkList

---
+++ src/components/TalkList.vue
@@ -0,0 +1,33 @@
+<template>
+    <md-list class="md-double-line">
+        <md-list-item>
+            <md-ink-ripple />
+            <md-avatar class="md-large">
+                <img src="https://placeimg.com/40/40/people/1" alt="People">
+            </md-avatar>
+            <div class="md-list-text-container">
+                <div class="title">
+                    <span class="md-title">Ali Connors</span>
+                    <span>Fri</span>
+                </div>
+                <span>Tody is a good day.</span>
+            </div>
+            <md-divider></md-divider>
+        </md-list-item>
+    </md-list>
+</template>
+
+<script>
+export default {
+  name: 'talkList',
+};
+</script>
+
+<style lang="scss" scoped>
+@import '~compass-mixins/compass/css3';
+
+.title {
+    @include display-flex;
+    @include justify-content(space-between);
+}
+</style>
