10fa2fead3602cbcf235202261b1c31ab10a5e3b
Randy Ou <ourd2012@gmail.com>
2017-06-03 01:41:24 +0800

    Add types and delete types ,add annotation.

--- src/store/types.js
+++ src/store/types.js
@@ -1,9 +1,10 @@
+// 更新当前联系人
 export const UPDATE_CURRENT_CONTACT = 'UPDATE_CURRENT_CONTACT';
-
+// 添加聊天记录
 export const ADD_CHAT_RECORD = 'ADD_CHAT_RECORD';
-
-export const SEND_CHAT_MESSAGE = 'SEND_CHAT_MESSAGE';
-
-export const UPDATE_CURRENT_PROFILE = 'update_current_profile';
-
+// 更新当前的profile
+export const UPDATE_CURRENT_PROFILE = 'UPDATE_CURRENT_PROFILE';
+// 更新聊天列表
 export const UPDATE_CHAT_LIST = 'UPDATE_CHAT_LIST';
+// 更新聊天记录为已读
+export const UPDATE_CHAT_RECORD = 'UPDATE_CHAT_RECORD';
