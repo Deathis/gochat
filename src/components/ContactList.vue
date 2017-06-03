60145c7c18ad9f6fef17f2a51fd855054b772e90
Randy Ou <ourd2012@gmail.com>
2017-06-04 00:40:50 +0800

    Change contact list avatar's radius to 0.

--- src/components/ContactList.vue
+++ src/components/ContactList.vue
@@ -1,16 +1,16 @@
 <template>
-    <md-list>
-        <md-list-item v-for="contact in contactList" :key="contact.account" @click.native="gotoProfile(contact)">
-            <md-ink-ripple />
-            <md-avatar class="md-large">
-                <img :src="contact.avatar" alt="Avatar">
-            </md-avatar>
-            <div class="md-list-text-container">
-                <span class="md-title">{{contact.name}}</span>
-                <md-divider></md-divider>
-            </div>
-        </md-list-item>
-    </md-list>
+  <md-list>
+    <md-list-item v-for="contact in contactList" :key="contact.account" @click.native="gotoProfile(contact)">
+      <md-ink-ripple />
+      <md-avatar class="md-large">
+        <img :src="contact.avatar" alt="Avatar">
+      </md-avatar>
+      <div class="md-list-text-container">
+        <span class="md-title">{{contact.name}}</span>
+        <md-divider></md-divider>
+      </div>
+    </md-list-item>
+  </md-list>
 </template>
 
 <script>
@@ -32,3 +32,11 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+.md-list-item {
+  .md-avatar {
+    border-radius: 0;
+  }
+}
+</style>
