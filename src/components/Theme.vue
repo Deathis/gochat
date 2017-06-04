7b358e3f95ad7066abb1d6155160aa8eb7de9613
Randy Ou <ourd2012@gmail.com>
2017-06-04 15:53:35 +0800

    Modify theme component's style.

--- src/components/Theme.vue
+++ src/components/Theme.vue
@@ -1,16 +1,16 @@
 <template>
-    <md-dialog ref="theme">
-        <md-dialog-title>Theme</md-dialog-title>
-    
-        <md-dialog-actions> 
-            <md-button-toggle md-single>
-                <md-button md-theme="default" class="md-primary" :class="{'md-toggle':currentTheme==='default'}" @click.native="closeDialog('default')">Default</md-button>
-                <md-button md-theme="green" class="md-primary" :class="{'md-toggle':currentTheme==='green'}" @click.native="closeDialog('green')">Green</md-button>
-                <md-button md-theme="teal" class="md-primary" :class="{'md-toggle':currentTheme==='teal'}" @click.native="closeDialog('teal')">Teal</md-button> 
-                <md-button md-theme="purple" class="md-primary" :class="{'md-toggle':currentTheme==='purple'}" @click.native="closeDialog('purple')">Purple</md-button> 
-            </md-button-toggle>
-        </md-dialog-actions>
-    </md-dialog>
+  <md-dialog ref="theme">
+    <md-dialog-title>Theme</md-dialog-title>
+  
+    <md-dialog-actions>
+      <md-button-toggle md-single>
+        <md-button md-theme="default" class="md-primary" :class="{'md-toggle':currentTheme==='default'}" @click.native="closeDialog('default')">Default</md-button>
+        <md-button md-theme="green" class="md-primary" :class="{'md-toggle':currentTheme==='green'}" @click.native="closeDialog('green')">Green</md-button>
+        <md-button md-theme="teal" class="md-primary" :class="{'md-toggle':currentTheme==='teal'}" @click.native="closeDialog('teal')">Teal</md-button>
+        <md-button md-theme="purple" class="md-primary" :class="{'md-toggle':currentTheme==='purple'}" @click.native="closeDialog('purple')">Purple</md-button>
+      </md-button-toggle>
+    </md-dialog-actions>
+  </md-dialog>
 </template>
 
 <script>
@@ -33,3 +33,14 @@ export default {
   ]),
 };
 </script>
+
+<style lang="scss" scoped>
+.md-dialog-actions {
+  .md-button {
+    margin-left: 0;
+  }
+  .md-button-toggle {
+    flex-wrap: wrap;
+  }
+}
+</style>
