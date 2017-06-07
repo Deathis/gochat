30d2f77438054e4ae5eed47ced6a4089c2fc6d81
Randy Ou <ourd2012@gmail.com>
2017-06-07 23:18:32 +0800

    Change some text.

--- src/components/MyProfile.vue
+++ src/components/MyProfile.vue
@@ -68,12 +68,12 @@
         <md-dialog ref="editGender">
             <md-dialog-title>Edit Gender</md-dialog-title>
             <div class="gender-group">
-                <md-radio class="md-primary" v-model="gender" id="my-test1" name="gender" md-value="0">Male</md-radio>
-                <md-radio class="md-primary" v-model="gender" id="my-test2" name="gender" md-value="1">Female</md-radio>
+                <md-radio class="md-primary" v-model="gender" name="gender" md-value="0">Male</md-radio>
+                <md-radio class="md-primary" v-model="gender" name="gender" md-value="1">Female</md-radio>
             </div>
             <md-dialog-actions>
                 <md-button class="md-primary" @click.native="closeEditGenderDialog('cancel')">Cancel</md-button>
-                <md-button class="md-primary" @click.native="closeEditGenderDialog('ok')">Ok</md-button>
+                <md-button class="md-primary" @click.native="closeEditGenderDialog('ok')">Done</md-button>
             </md-dialog-actions>
         </md-dialog>
     </md-layout>
