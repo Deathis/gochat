9f7f60b68887928736f940b77dcabf6619e17e0b
Randy Ou <ourd2012@gmail.com>
2017-06-20 21:13:48 +0800

    修复饮用。

--- src/components/ChatList.vue
+++ src/components/ChatList.vue
@@ -12,7 +12,7 @@
                 </div>
                 <span>
                     <template v-if="chat.conversation.unreadMessagesCount">[{{chat.conversation.unreadMessagesCount}} message(s)]</template> 
-                    {{chat.conversation.lastMessage?chat.conversation.lastMessage._lctext:''}}
+                    {{chat.conversation.lastMessage?chat.conversation.lastMessage.text:''}}
                 </span>
             </div>
             <md-divider></md-divider>
